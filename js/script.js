95562a977403118f09eac5e885507c152c887d9c
Betty Ballin <betty.ballin@yahoo.de>
2017-05-06 15:53:39 +0200

    initialized text files

--- js/script.js
+++ js/script.js
@@ -1,34 +1,17 @@
 jQuery(function($){
     $('body').on('mouseenter', 'a', function(e) {
         var o = this;
-        if ( o.href != '#' ) {
+        if ( o.href != '#' && o.href != 'javascript:void(0)' && o.id != 'torpedoHref') {
             chrome.extension.sendRequest('show', function(r) {
-                var uri = $.url.parse(o.href),
-                    position,
-                    text = uri.source.replace(uri.host, uri.host.replace(extractDomain(uri.host),"")+'<span style="color:#0033cc">' + extractDomain(uri.host) + '</span>');
-                    position = {
-                        at: 'center bottom',
-                        my: 'top left',
-                        target: 'mouse',
-                        viewport: $(window),
-                        adjust: {
-                          mouse: false
-                        }
-                    }
                 // Is the target a new window?
-                if ( $(o).attr('target') == '_blank' ) text = '<i class="fa fa-external-link-square" style="padding-right: 5px;"></i>' + text;
+                //if ( $(o).attr('target') == '_blank' ) ...
 
-                var frame = document.createElement("IFRAME");
-                frame.src = chrome.extension.getURL("torpedo.html");
-	              frame.setAttribute('width', '300px');
-	              frame.setAttribute('height', '50px');
-                frame.onload = function(){
-                  iframeWindow.document.body.prepend("Hello, world!");
-                };
                 // Show the qtip
                 $(o).qtip({
                     overwrite: false,
-                    content:  $(frame),
+                    content:  {
+                      text: tooltipText(o.href)
+                  },
                     show: {
                         event: e.type,
                         ready: true,
@@ -36,10 +19,29 @@ jQuery(function($){
                     },
                     hide: {
                         fixed: true,
-                        delay: 1000
+                        delay: 1500
                     },
-                    position: position,
-                    style: { classes: 'torpedoTooltip' }
+                    position: {
+                        at: 'center bottom',
+                        my: 'top left',
+                        target: 'mouse',
+                        viewport: $(window),
+                        adjust: {
+                          mouse: false
+                        }
+                    },
+                    style: { classes: 'torpedoTooltip' },
+                    events: {
+                      render: function(event, api) {
+                        //var iframe = $('iframe', this)[0];
+                        //var tooltip = $(this);
+                        //$('<p id="torpedoSecurityStatus">'+chrome.i18n.getMessage("waitingTime", "3")+'</p>').appendTo(api.elements.content);
+                        console.log(api.elements.content.html());
+                      },
+                      hide: function(event, api) {
+                        //console.log("hide");
+                      }
+                    }
                 }, e);
             })
         }
@@ -48,8 +50,59 @@ jQuery(function($){
     })
 });
 
+/**
+* get domain out of the URI
+* URI is the original URL cut off after the domain
+*/
 function extractDomain(domain){
   var split = domain.split(".");
   if(split.length > 2) domain = split[split.length-2]+"."+split[split.length-1];
   return domain;
 };
+
+/**
+* fill tooltip with text
+*/
+function tooltipText(href){
+  var uri = $.url.parse(href);
+  var domain = extractDomain(uri.host);
+  var top = "";
+  var url = '<a href="'+uri.source+'" id="torpedoURL">' + uri.source.replace(uri.host, '<span id="torpedoDomain">' + domain + '</span>') + '</a>';
+  var securityStatus = "";
+  var warning = "";
+  var advice = "";
+  var wait = "<p id='torpedoLinkDelay'>" + chrome.i18n.getMessage("linkDelay") + '</p>';
+  var timer = "<p id='torpedoTimer'>" + chrome.i18n.getMessage("waitingTime", "3") + '</p>';
+  // TODO: timer function, local user settings
+
+  switch(getSecurityStatus(url)){
+    case "lowrisk":
+      securityStatus = "<p id='torpedoSecurityStatus'>" + chrome.i18n.getMessage("unknownDomain") + '</p>';
+      wait = ""; timer = "";
+      // TODO: connect waitingTime with user settings
+      break;
+    case "userdefined":
+      securityStatus = "<p id='torpedoSecurityStatus'>" + chrome.i18n.getMessage("userDefinedDomain") + '</p>';
+      wait = ""; timer = "";
+      // TODO: connect waitingTime with user settings
+      break;
+    case "lowrisk":
+      securityStatus = "<p id='torpedoSecurityStatus'>" + chrome.i18n.getMessage("lowRiskDomain") + '</p>';
+      break;
+    case "redirect":
+      securityStatus = "<p id='torpedoSecurityStatus'>" + chrome.i18n.getMessage("redirectDomain") + '</p>';
+      break;
+    case "encrypted":
+      top = "<p id='top'>" + chrome.i18n.getMessage("encryptedDomain") + '</p>';
+      // TODO: switch case to other security statuses
+      break;
+    case "phish":
+      warning = '<p id="warning"'+chrome.i18n.getMessage("phishWarning") + '</p>';
+      // TODO: 2 more secs for phish case timer
+  }
+  return ""+top+url+securityStatus+warning+advice+wait+timer;
+};
+
+function getSecurityStatus(domain){
+  return "unknown";
+}
