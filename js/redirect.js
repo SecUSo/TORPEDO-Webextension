d0a360f8acc1c54b98b6c02726d6ad99869fea45
lauraimler <24718871+lauraimler@users.noreply.github.com>
2022-03-31 13:53:39 +0200

    Loader integration
    Add loader for when the tooltip is being prepared or for when data is fetched, e.g. for shortURL calls

--- js/redirect.js
+++ js/redirect.js
@@ -2,6 +2,7 @@
 *   resolve a redirection url, f.e. tinyurl
 */
 function resolveRedirect(event) {
+  onlyShowLoader();
   chrome.runtime.sendMessage({ name: "redirect", url: torpedo.url }, function (r) {
     torpedo.api.set("hide.event", "unfocus");
     torpedo.api.set("hide.delay", 0);
