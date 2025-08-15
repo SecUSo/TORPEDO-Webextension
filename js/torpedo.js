23396cc8e467972936c3ec92f712759ce7f833f5
max <uqwfa@student.kit.edu>
2025-08-15 20:59:29 +0200

    add comments to clarify torpedo attributes

--- js/torpedo.js
+++ js/torpedo.js
@@ -1,20 +1,20 @@
 const torpedo = {
-    api: null,  // tippy instance
+    api: null,
     countRedirect: 0,
     countShortURL: 0,
-    domain: "",
+    domain: "",  // the domain of the target URL
     event: null,
     hasTooltip: false,
     location: null,
     oldDomain: "",
     oldUrl: "",
     opened: false,
-    pathname: "",
+    pathname: "",  // the pathname of the target URL after the hostname
     progUrl: false,
     publicSuffixList: "",
     state: "unknown",
     target: null,  // target element for which the tooltip should be shown
     tooltip: null,  // tippy html content
-    uri: "",
-    url: ""
+    uri: "",  // URI object fot the target URL with protocol, host, port, pathname, search, hash, ...
+    url: ""  // the complete URL of the target element
 }
