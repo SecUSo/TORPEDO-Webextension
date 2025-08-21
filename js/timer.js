ae489549498ad906365e13d809e35bbffdd00fb7
Max <uqwfa@student.kit.edu>
2025-08-21 18:16:32 +0200

    refactor timer.js to improve event handling and enhance timer functionality

--- js/timer.js
+++ js/timer.js
@@ -1,113 +1,93 @@
-torpedo.timerInterval = null;
-
-function preventClickEvent(eventTarget, eventTypes) {
-  eventTypes.forEach(function (eventType) {
-    $(eventTarget).unbind(eventType);
-  });
-
-  eventTypes.forEach(function (eventType) {
-    $(eventTarget).on(eventType, function (event) {
-      event.preventDefault();
-      return false;
-    });
-  });
-}
-
-function reactivateLink(eventTarget, eventTypes) {
-  eventTypes.forEach(function (eventType) {
-    $(eventTarget).unbind(eventType);
-  });
-
-  $(torpedo.target).bind("click", function (event) {
-    processClick();
-  });
-}
-
-function reactivateTooltipURL(tooltipURL) {
-  try {
-    const clone = tooltipURL.cloneNode(true);
-    tooltipURL.parentNode.replaceChild(clone, tooltipURL);
-
-    tooltipURL.addEventListener("click", async function (event) {
-      event.preventDefault()
-      const storage = await browser.storage.sync.get();
-      const urlToOpen = storage.privacyModeActivated ? torpedo.oldUrl : torpedo.url;
-      browser.runtime.sendMessage({name: "open", url: urlToOpen});
-      processClick();
-      return false;
-    })
-
-  } catch (e) {
-    console.log(e)
-  }
-}
-
-function isTimerActivated(storage, securityStatus) {
-  switch (securityStatus) {
-    case "T1":
-      return storage.trustedTimerActivated;
-    case "T2":
-      return storage.userTimerActivated;
-    default:
-      return true;
-  }
+/**
+ * Prevents the default action of an event.
+ */
+function preventDefaultHandler(event) {
+    event.preventDefault();
+    event.stopPropagation();
 }
 
 /**
- * countdown function for the temporal deactivation of URLs
+ * Prevents click events on a target element.
  */
+function preventClickEvent(eventTarget, eventTypes) {
+    eventTypes.forEach(eventType => {
+       eventTarget.addEventListener(eventType, preventDefaultHandler, { capture: true });
+    });
+}
 
-function countdown(time, state, clickLinkEventTypes) {
-  if (torpedo.target.classList.contains("torpedoTimerFinished")) time = 0;
+/**
+ * Re-enables click events on a target element.
+ */
+function reactivateLink(eventTarget, eventTypes) {
+    eventTypes.forEach(eventType => {
+        eventTarget.removeEventListener(eventType, preventDefaultHandler, { capture: true });
+    });
 
-  const tooltip = torpedo.tooltip;
+    eventTarget.addEventListener("click", () => TooltipManager.processClick());
+}
 
-  const timerEl = tooltip.querySelector("#torpedoTimer");
-  timerEl.style.display = "block";
-
-  /**
-   * assert time to tooltip text
-   */
-  function showTime() {
+/**
+ * Re-enables the tooltip URL link.
+ */
+function reactivateTooltipURL(tooltipURL) {
     try {
-      const t = tooltip.querySelector("#torpedoTimer");
-      t.textContent = browser.i18n.getMessage("verbleibendeZeit", "" + time);
-    } catch (e) {}
-  }
+        const clone = tooltipURL.cloneNode(true);
+        tooltipURL.parentNode.replaceChild(clone, tooltipURL);
 
-  torpedo.target.classList.add("torpedoTimerShowing");
-  
+        tooltipURL.addEventListener("click", async (event) => {
+            event.preventDefault()
+            const storage = await browser.storage.sync.get();
+            const urlToOpen = storage.privacyModeActivated ? torpedo.oldUrl : torpedo.url;
+            browser.runtime.sendMessage({ name: "open", url: urlToOpen });
+            TooltipManager.processClick();
+        });
 
-  const onWebsite = new URL(window.location.href);
-  if (onWebsite.hostname === "owa.kit.edu") {
-    document.querySelectorAll("div._rp_U4.ms-font-weight-regular.ms-font-color-neutralDark.rpHighlightAllClass.rpHighlightBodyClass").
-    forEach(el => {
-      const clone = el.cloneNode(true);
-        el.parentNode.replaceChild(clone, el);
-    })
-    // document.removeEventListener('click', getEventListeners(document).click[0].listener)
-    /* 
-      once script from owa can be used to remove eventlistener properly - insert here
-    */
-  }
-
-  showTime();
-  if (time > 0) time--;
-
-  const timerInterval = setInterval(function timer() {
-    showTime();
-    if (time === 0) {
-      clearInterval(timerInterval);
-      if (!isRedirect(torpedo.domain) && state !== "T4" && state !== "T4a") {
-        torpedo.target.classList.add("torpedoTimerFinished");
-      }
-
-      reactivateLink(torpedo.target, clickLinkEventTypes);
-      reactivateTooltipURL(tooltip.querySelector("#torpedoURL"));
-    } else {
-      --time;
+    } catch (e) {
+        console.log(e)
     }
-  }, 1000);
+}
 
-  torpedo.timerInterval = timerInterval;
+/**
+ * Checks if the timer is activated for a given security status.
+ */
+function isTimerActivated(storage, securityStatus) {
+    switch (securityStatus) {
+        case "T1": return storage.trustedTimerActivated;
+        case "T2": return storage.userTimerActivated;
+        default: return true;
+    }
+}
+
+/**
+ * Starts a countdown timer to delay link activation.
+ */
+function countdown(time, state, clickLinkEventTypes) {
+    if (torpedo.target.classList.contains("torpedoTimerFinished")) time = 0;
+
+    const timerEl = torpedo.tooltip.querySelector("#torpedoTimer");
+    timerEl.style.display = "block";
+
+    const updateTimerText = (remainingTime) => {
+        timerEl.textContent = browser.i18n.getMessage("verbleibendeZeit", "" + remainingTime);
+    }
+
+    updateTimerText(time);
+    torpedo.target.classList.add("torpedoTimerShowing");
+
+    if (time > 0) time--;
+
+    torpedo.timerInterval = setInterval(async () => {
+        updateTimerText(time);
+        if (time === 0) {
+            clearInterval(torpedo.timerInterval);
+            if (!await isRedirect(torpedo.domain) && state !== "T4" && state !== "T4a") {
+                torpedo.target.classList.add("torpedoTimerFinished");
+            }
+
+            reactivateLink(torpedo.target, clickLinkEventTypes);
+            reactivateTooltipURL(torpedo.tooltip.querySelector("#torpedoURL"));
+        } else {
+            time--;
+        }
+    }, 1000);
 }
