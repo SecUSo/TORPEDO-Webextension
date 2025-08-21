489edb999d79a406e437aaf99ff3e572c25c434c
Max <uqwfa@student.kit.edu>
2025-08-21 18:15:54 +0200

    refactor redirect.js to enhance URL resolution logic

--- js/redirect.js
+++ js/redirect.js
@@ -1,90 +1,81 @@
 /**
-*   resolve a redirection url, f.e. tinyurl
-*/
-function resolveRedirect(event) {
-  showLoaderWithOverlay();
-  chrome.runtime.sendMessage({ name: "redirect", url: torpedo.url }, function (r) {
-    torpedo.api.set("hide.event", "unfocus");
-    torpedo.api.set("hide.delay", 0);
-    try {
-      const href = new URL(r);
-      setNewUrl(href);
-      updateTooltip();
-    } catch (e) { console.log(e) }
-  });
-};
+ * Resolves a redirect URL (e.g., a short URL).
+ */
+function resolveRedirect() {
+    TooltipManager.showLoaderWithOverlay()
+    browser.runtime.sendMessage({ name: "redirect", url: torpedo.url })
+        .then(response => {
+            try {
+                const href = new URL(response);
+                TooltipManager.setNewUrl(href);
+                TooltipManager.updateTooltip();
+            } catch (e) { }
+        })
+        .catch(error => console.error(error));
+}
 
 /**
-*   resolve a referrer url, f.e. https://deref-gmx.net/mail/client/..
-*/
-function resolveReferrer(r) {
-  var url = torpedo.url;
-  var arr1 = r.referrerPart1;
-  var arr2 = r.referrerPart2;
-  var arr3 = r.referrerPart3;
+ *  Resolves a referrer URL.
+ */
+function resolveReferrer(storage) {
+    const { referrerPart1, referrerPart2, referrerPart3 } = storage;
+    if (!referrerPart1 || !referrerPart2 || !referrerPart3) return;
 
-  for (var i = 0; i < (arr1.length + arr2.length); i++) {
-    if (url.indexOf(arr1[i]) > -1) {
-      var cut = arr3[i] ? arr3[i] : arr2[i];
-      var index = url.indexOf(cut);
-      var temp = url.substring(index + cut.length, url.length);
-      temp = decodeURIComponent(temp);
-      try {
-        const href = new URL(temp);
-        setNewUrl(href);
-      } catch (e) { }
-      break;
+    for (let i = 0; i < referrerPart1.length; i++) {
+        if (torpedo.url.includes(referrerPart1[i])) {
+            const cut = referrerPart3[i] || referrerPart2[i];
+            const index = torpedo.url.indexOf(cut);
+            if (index !== -1) {
+                let temp = torpedo.url.substring(index + cut.length);
+                temp = decodeURIComponent(temp);
+                try {
+                    const href = new URL(temp);
+                    TooltipManager.setNewUrl(href);
+                    return;
+                } catch (e) { }
+                break;
+            }
+        }
     }
-  }
-};
+}
 
 
-/**
+/*
  * checks if the current url is a referrer 
- * @param url
+ *
  * @return resolved referrer or <NO_RESOLVED_REFERRER> if the current url is no referrer or there was an error 
  */
-function matchReferrer(url) {
-  const href = new URL(url);
-  var hostnameURL = href.hostname;
+function matchReferrer(url, storage) {
+    const {referrerPart1, referrerPart2, referrerPart3} = storage;
+    if (!referrerPart1 || !referrerPart2 || !url) {
+        return "<NO_RESOLVED_REFERRER>";
+    }
 
-  var referrerDomainArr = r.referrerPart1;
-  var referrerPathArr = r.referrerPart2;
-  var referrerAttributeArr = r.referrerPart3;
+    const href = new URL(url);
+    const hostnameURL = href.hostname;
 
-  if (referrerDomainArr === null || referrerPathArr === null || !hostnameURL) {
+    const indices = referrerPart1
+        .map((element, i) => {
+            const domainParts = element.split("*").filter(Boolean);
+            return domainParts.every(part => hostnameURL.includes(part)) ? i : -1;
+        })
+        .filter(index => index !== -1);
+
+    for (const index of indices) {
+        const pathParts = referrerPart2[index].split("[...]").filter(Boolean);
+        if (pathParts.every(part => url.includes(part))) {
+            const cut = referrerPart3[index];
+            const urlAttrIndex = url.indexOf(cut);
+            if (urlAttrIndex !== -1) {
+                let temp = url.substring(urlAttrIndex + cut.length);
+                temp = decodeURIComponent(temp);
+                if (temp.startsWith("http") || temp.startsWith("www")) {
+                    return temp;
+                }
+            }
+        }
+    }
+
     return "<NO_RESOLVED_REFERRER>";
-  }
-
-  var indices = referrerDomainArr.map(function (element, i) {
-    var domainParts = element.split("*").filter(String);
-    return domainParts.every(function (el) {
-      return hostnameURL.includes(el);
-    }, hostnameURL) ? i : '';
-  }, hostnameURL).filter(String);
-
-  for (var i = indices.length - 1; i >= 0; i--) {
-    var ind = indices[i];
-    var pathParts = referrerPathArr[ind].split("[...]").filter(String);
-    for (path of pathParts) {
-      if (!url.includes(path)) {
-        indices = indices.splice(i, 1);
-        break;
-      }
-    }
-  }
-
-  for (index of indices) {
-    var cut = referrerAttributeArr[index];
-    var urlAttrIndex = url.indexOf(cut);
-    var temp = url.substring(urlAttrIndex + cut.length, url.length);
-    temp = decodeURIComponent(temp);
-    if (temp.startsWith("http") || temp.startsWith("https") || temp.startsWith("www")) {
-      url = temp;
-      return url;
-    }
-
-  }
-  return "<NO_RESOLVED_REFERRER>";
 }
 
