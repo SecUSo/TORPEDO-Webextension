8737434edcb93e136de839ddcd5107f3dc138f41
max <uqwfa@student.kit.edu>
2026-07-09 10:34:36 +0200

    fix re for class extraction

--- src/shared/js/utils.js
+++ src/shared/js/utils.js
@@ -100,7 +100,7 @@ const Utils = (function () {
      */
     function parseLimitedMarkup(str) {
         const frag = document.createDocumentFragment();
-        const re = /<br\s*\/?>|<span\s+([^>]*)>(.*?)<\/span>/gis;
+        const re = /<br\s*\/?>|<span\s+class=['"]([^'"]+)['"]>(.*?)<\/span>/gis;
         let lastIndex = 0;
         let match;
 
